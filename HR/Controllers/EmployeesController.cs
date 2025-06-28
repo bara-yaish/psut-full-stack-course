5101401776895476419e69b1a8b1e35cc5e2ab3c
byaish <byaish@e2abs.com>
2025-06-28 13:53:54 +0300

    Add new Core Web API project

---
+++ HR/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using HR.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.Controllers
+{
+    // Data Annotation - Extra information given to a class, method or property
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        List<Employee> employeesList =
+        [
+            new () { Name = "Bara' Yaish", Age = 23, Position = "Developer" },
+            new () { Name = "Omar Yaish", Age = 23, Position = "Developer" },
+            new () { Name = "Someone", Age = 25, Position = "HR" },
+            new () { Name = "Someone else", Age = 23, Position = "Manager" },
+        ];
+
+        [HttpGet("GetEmployees")]
+        public IActionResult GetEmployee([FromQuery] string position)
+        {
+            var employees = from employee in employeesList
+                            where employee.Position == position
+                            orderby employee.Age
+                            select new 
+                            { 
+                                employee.Name,
+                                employee.Position,
+                            };
+
+            return Ok(employees);
+        }
+    }
+}
---
+++ HR/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+    }
+}
