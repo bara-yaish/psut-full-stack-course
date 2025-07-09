902c5d1617132baff89179a88aef247c9487e31c
bara-yaish <baraihab49@gmail.com>
2025-07-09 22:30:48 +0300

    Create filter dto for employees

---
+++ HR/DTOs/Employees/FilterEmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.DTOs.Employees
+{
+    public class FilterEmployeeDto
+    {
+        public string? Name { get; set; }
+        public string? Position { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
