dc793120f31470b5559bab96b0102888200394aa
bara-yaish <baraihab49@gmail.com>
2025-07-09 22:28:37 +0300

    Add necessary fields to return useful data to the user

--- HR/DTOs/Employees/EmployeeDto.cs
+++ HR/DTOs/Employees/EmployeeDto.cs
@@ -5,8 +5,13 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public int? Age { get; set; }
+        public string? Phone { get; set; }
         public string Position { get; set; }
         public bool IsActive { get; set; }
         public DateTime StartDate { get; set; }
+        public long? ManagerId { get; set; }
+        public string? ManagerName { get; set; }
+        public long? DepartmentId { get; set; }
+        public string? DepartmentName { get; set; }
     }
 }
