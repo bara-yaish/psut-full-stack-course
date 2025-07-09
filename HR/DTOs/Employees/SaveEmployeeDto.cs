72e42ce0e549b24f84bad7ca9b9612a23c99eb73
bara-yaish <baraihab49@gmail.com>
2025-07-09 22:30:14 +0300

    Add missing fields to match the Employee model and the Employee table in the database

--- HR/DTOs/Employees/SaveEmployeeDto.cs
+++ HR/DTOs/Employees/SaveEmployeeDto.cs
@@ -5,10 +5,12 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public int? Age { get; set; }
-        public string Position { get; set; }
         public string? Phone { get; set; }
+        public string Position { get; set; }
         public bool IsActive { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+        public long? ManagerId { get; set; }
+        public long? DepartmentId { get; set; }
     }
 }
