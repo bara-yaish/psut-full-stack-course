cf5de9f534f46f2409f504c66fe045b68806a862
bara-yaish <baraihab49@gmail.com>
2025-07-09 22:27:36 +0300

    Add missing fields to match the Employees table in the database

--- HR/Models/Employee.cs
+++ HR/Models/Employee.cs
@@ -10,5 +10,7 @@
         public bool IsActive { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+        public long? DepartmentId { get; set; }
+        public long? ManagerId { get; set; }
     }
 }
