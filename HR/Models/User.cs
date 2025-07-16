f98ed749470d92675e80ab911ac60ae5dcf1cc82
bara-yaish <baraihab49@gmail.com>
2025-07-16 21:59:16 +0300

    Create new Users model

---
+++ HR/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string HashedPassword { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
