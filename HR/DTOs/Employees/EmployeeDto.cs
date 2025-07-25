eac2560e3a365ca112252b8668f49d01a8ce78c7
byaish <byaish@e2abs.com>
2025-07-25 22:07:05 +0300

    Modify existing Position field to match Employees Model

--- HR/DTOs/Employees/EmployeeDto.cs
+++ HR/DTOs/Employees/EmployeeDto.cs
@@ -5,12 +5,14 @@
         public long Id { get; set; }
         public string Name { get; set; }
         public string? Phone { get; set; }
-        public string Position { get; set; }
+        public DateTime? BirthDate { get; set; }
         public bool IsActive { get; set; }
         public DateTime StartDate { get; set; }
         public long? ManagerId { get; set; }
         public string? ManagerName { get; set; }
         public long? DepartmentId { get; set; }
         public string? DepartmentName { get; set; }
+        public long? PositionId { get; set; }
+        public string? PositionName { get; set; }
     }
 }
--- HR/DTOs/Employees/SaveEmployeeDto.cs
+++ HR/DTOs/Employees/SaveEmployeeDto.cs
@@ -4,9 +4,8 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public int? Age { get; set; }
-        public string? Phone { get; set; }
-        public string Position { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public long? PositionId { get; set; }
         public bool IsActive { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
