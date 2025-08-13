579f744dd0f36e0a324b8896b9b5d423b20c7cb8
bara-yaish <baraihab49@gmail.com>
2025-08-13 22:02:07 +0300

    Interpolation and directives notes

--- HR/Frontend/HR/src/app/app.ts
+++ HR/Frontend/HR/src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf, NgFor, NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor, NgClass, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HR');
+  title = "Welcome to Angular from Typescript";
+  num: number = 52.165;
+  bool: boolean = true;
+  arr: string[] = ["one", "two", "three"]
+
+  students = [
+    { name: "Bara", mark: 49 },
+    { name: "Aya", mark: 56 },
+    { name: "Omar", mark: 41 },
+    { name: "Rula", mark: 95 },
+    { name: "Ihab", mark: 68 },
+    { name: "Anas", mark: 30 },
+  ]
 }
